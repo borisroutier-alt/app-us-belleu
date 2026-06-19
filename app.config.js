699482ed5048831344d4f4dd61e0a5fe0d882888
Boris <boris.routier@gmail.com>
2026-06-20 01:23:08 +0200

    fix: chemins relatifs

--- app.config.js
+++ app.config.js
@@ -5,14 +5,7 @@ module.exports = {
     web: {
       bundler: "metro",
       output: "static",
-      // C'est ici que tu dois définir le chemin relatif pour le sous-dossier
-      // Expo Router l'utilisera pour construire ses routes internes
-      baseUrl: "/app-us-belleu", 
-    },
-    extra: {
-      router: {
-        origin: false,
-      },
+      basePath: "/app-us-belleu", // C'est ici que ça se joue
     },
   },
 };
