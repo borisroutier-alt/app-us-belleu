eb0537b7537490b9aa2e39bf4f40921be43e0ca5
borisroutier-alt <boris.routier@gmail.com>
2026-06-24 16:34:12 +0000

    Deploying to main from @ borisroutier-alt/app-us-belleu@0abf1bd90a5c5851462b2e73d133d4397d5ed46e 🚀

--- hermes-polyfill.js
+++
@@ -1,7 +0,0 @@
-// Ce script vient rassurer le compilateur Hermes en lui faisant croire 
-// que la fonction "import()" dynamique est gérée de manière basique.
-if (typeof global.import === 'undefined') {
-  global.import = function () {
-    return Promise.resolve({});
-  };
-}
